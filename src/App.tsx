1def17bfa58b5f849b5a39a5af6f98bda846493d
Lucas Souza <lucass.souza@hotmail.com>
2022-10-07 21:15:26 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+
+import './App.css'; 
+import { AppRoutes } from "./Routes";
+
+function App() {
+  // const [tweets, setTweets] = useState<string[]>([
+  //   'tweet 1',
+  //   'tweet 2',
+  //   'tweet 3',
+  //   'tweet 4',
+  // ])
+
+  // function createTweet(){
+  //   setTweets([... tweets, 'tweet 5'])
+  // }
+
+  return (
+    <AppRoutes/>
+    // <div>
+    //   {tweets.map(tweet => {
+    //     return <Tweet text={tweet}/> 
+    //   })}
+
+    //   <button onClick={createTweet}
+    //     style={{
+    //       backgroundColor: '#8257e6',
+    //       border: 0,
+    //       padding: '6px 12px',
+    //       color: '#fff'
+    //     }}
+        
+        
+    //     >Adicionar tweets</button>
+    // </div>
+  )
+}
+
+export default App
---
+++ src/Routes.tsx
@@ -0,0 +1,21 @@
+import {
+      BrowserRouter as Router,
+      Routes,
+      Route
+} from "react-router-dom"
+
+import { Cart } from "./pages/Cart"
+import { Catalogo } from "./pages/Catalogo"
+import { Login } from "./pages/Login"
+
+export function AppRoutes() {
+      return (
+            <Router>
+                  <Routes>
+                        <Route path="/Login" element={<Login/>}/>
+                        <Route path="/Cart" element={<Cart/>}/>
+                        <Route path="/Catalogo" element={<Catalogo/>}/>
+                  </Routes>
+            </Router>
+      )
+}
