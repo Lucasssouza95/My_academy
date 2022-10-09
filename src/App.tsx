4b4981db92d49e73589e6695f31bf3736c2e4391
Lucas Souza <lucass.souza@hotmail.com>
2022-10-09 01:16:11 -0300

    iniciando authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState } from "react";
-import { Tweet } from "./components/Tweet";
+import { useState } from 'react'
+import { Tweet } from './components/Tweet'
 
-import './App.css'; 
-import { AppRoutes } from "./Routes";
+import './App.css'
+import { AppRoutes } from './Routes'
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
-  // const [tweets, setTweets] = useState<string[]>([
-  //   'tweet 1',
-  //   'tweet 2',
-  //   'tweet 3',
-  //   'tweet 4',
-  // ])
-
-  // function createTweet(){
-  //   setTweets([... tweets, 'tweet 5'])
-  // }
-
-  return (
-    <AppRoutes/>
-    // <div>
-    //   {tweets.map(tweet => {
-    //     return <Tweet text={tweet}/> 
-    //   })}
-
-    //   <button onClick={createTweet}
-    //     style={{
-    //       backgroundColor: '#8257e6',
-    //       border: 0,
-    //       padding: '6px 12px',
-    //       color: '#fff'
-    //     }}
-        
-        
-    //     >Adicionar tweets</button>
-    // </div>
-  )
+    return (
+        <AuthProvider>
+            <AppRoutes />
+        </AuthProvider>
+    )
 }
 
 export default App
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,20 @@
-import {
-      BrowserRouter as Router,
-      Routes,
-      Route
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ProtectedLayout } from './components/ProtectedLayout'
 
-import { Cart } from "./pages/Cart"
-import { Catalogo } from "./pages/Catalogo"
-import { Login } from "./pages/Login"
+import { Cart } from './pages/Cart'
+import { Catalogo } from './pages/Catalogo'
+import { Login } from './pages/Login'
 
 export function AppRoutes() {
-      return (
+    return (
+        <ProtectedLayout>
             <Router>
-                  <Routes>
-                        <Route path="/Login" element={<Login/>}/>
-                        <Route path="/Cart" element={<Cart/>}/>
-                        <Route path="/Catalogo" element={<Catalogo/>}/>
-                  </Routes>
+                <Routes>
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Cart" element={<Cart />} />
+                    <Route path="/Catalogo" element={<Catalogo />} />
+                </Routes>
             </Router>
-      )
+        </ProtectedLayout>
+    )
 }
