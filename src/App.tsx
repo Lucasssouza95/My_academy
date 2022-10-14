3e289733bd311205a03de5f66574a08397b7ccf3
Lucas Souza <lucass.souza@hotmail.com>
2022-10-14 00:08:51 -0300

    mudanca de planos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import { Tweet } from './components/Tweet'
 
 import './App.css'
 import { AppRoutes } from './Routes'
-import { AuthProvider } from './context/AuthProvider'
 
 function App() {
     return (
-        <AuthProvider>
-            <AppRoutes />
-        </AuthProvider>
+        <AppRoutes />
     )
 }
 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,22 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ProtectedLayout } from './components/ProtectedLayout'
+
 
 import { Cart } from './pages/Cart'
 import { Catalogo } from './pages/Catalogo'
-import { Login } from './components/Login/index'
+import { Login } from './pages/Login'
 
 export function AppRoutes() {
     return (
-        <ProtectedLayout>
+        
             <Router>
                 <Routes>
-                    <Route path="/Login">
-                        <Login/>
-                    </Route>
+                    <Route path="/Login" element={<Login/>} /> 
                     <Route path="/Cart" element={<Cart />} />
                     <Route path="/Catalogo" element={<Catalogo />} />
                 </Routes>
             </Router>
-        </ProtectedLayout>
+        
     )
 }
